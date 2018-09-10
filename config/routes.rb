a25ffa3c418e2f8f65b653d1db24e31e10c0a259
jiko797torayo <koji@ooishikoujinoMacBook-puro.local>
2018-09-10 19:46:37 +0900

    setting-routes-and-create-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'messages#index'
-  resources :users, only: [:update, :edit]
+  # root 'groups#index'
+  resources :users, only: [:index, :update, :edit]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
