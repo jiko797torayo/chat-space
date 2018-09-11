b817a2a3564e070710faf2692a24b80c407e0d76
jiko797torayo <koji@ooishikoujinoMacBook-puro.local>
2018-09-11 17:05:57 +0900

    root-change-group-index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
-  # root 'groups#index'
+  # root 'messages#index'
+  root 'groups#index'
   resources :users, only: [:index, :update, :edit]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
