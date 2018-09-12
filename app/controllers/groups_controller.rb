6f3266702207687f30102de466664f507bfbc75a
jiko797torayo <koji@ooishikoujinoMacBook-puro.local>
2018-09-12 10:47:02 +0900

    add-group-edit-function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,6 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
   def index
-
   end
 
   def new
@@ -19,7 +18,6 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def update
