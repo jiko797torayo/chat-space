9479b6e552a6c18cbf5bf2741222afa4dbf28577
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-09-14 10:08:32 +0900

    images-size-fixed
    ・投稿される画像が大きすぎるので縮小

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   storage :file
   include CarrierWave::MiniMagick
-  process resize_to_fit: [800, 800]
+  process resize_to_fit: [200, 200]
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
