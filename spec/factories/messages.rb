e732f406f74758e8696617d9af70d894a6bd49e1
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-09-16 19:20:57 +0900

    test

--- spec/factories/messages.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :message do
-    body Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/images/no_image.jpg")
-    user_id
-    group_id
-  end
-end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,7 @@
+require 'rails-helper'
+describe User do
+  describe '#create' do
+    it "is invalid"
+
+  end
+end
