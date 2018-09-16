1239bf3bda2f87b43ac00bfd62a11dbdbda17fd0
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-09-16 21:57:17 +0900

    write messages controller test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,7 +34,10 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+  #deviseをrspecで使用するためのコード
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include ControllerMacros, type: :controller
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
