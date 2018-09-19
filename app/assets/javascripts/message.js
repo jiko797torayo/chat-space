deb29aed31a2b434883af812242f054701201a27
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-09-19 20:23:41 +0900

    send message sequence

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -10,6 +10,17 @@ $(function(){
                 </div>`
     return html;
   }
+  function alertHTML(message){
+    var html = `<div class="main-messages-message">
+                  <div class="main-messages-message-user-name">${ message.user_name }</div>
+                  <div class="main-messages-message-created-at">${ message.created_at }</div>
+                  <div class="main-messages-message-body">
+                    ${ message.body }
+                  </div>
+                  <img class="message-image" src="${message.image}"
+                </div>`
+    return html;
+  }
   $('.message-form-for').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -26,6 +37,9 @@ $(function(){
         var html = buildHTML(data);
         $('.main-messages').append(html);
         $('.main-create-input').val('');
+        $('.image-file').val('');
+        $('.main-create-send').prop("disabled", false);
+        $('.main-messages').animate({scrollTop: $('.main-messages')[0].scrollHeight}, 'fast');
       })
       .fail(function() {
         alert('メッセージの送信に失敗しました');
