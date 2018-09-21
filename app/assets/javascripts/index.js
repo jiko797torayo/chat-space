aceffbbafb33e8fc77f176522079e54d4d93eafc
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-09-21 16:43:42 +0900

    add user function

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -37,4 +37,21 @@ $(function(){
         alert('メンバー検索に失敗しました');
       });
   });
+
+  var user_list = $("#chat-group-users");
+  function addUser(user_id, user_name){
+    var html = `<div class="chat-group-user">
+                    <input name="group[user_ids][]" type="hidden" value="${user_id}">
+                    <p class="chat-group-user__name">${user_name}</p>
+                    <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove">削除</a>
+                </div>`
+    user_list.append(html);
+  }
+
+  $(document).on("click", ".chat-group-user__btn--add", function(){
+    var user_id = $(this).attr('data-user-id');
+    var user_name = $(this).attr('data-user-name');
+    addUser(user_id, user_name);
+    $(this).parent().remove();
+  });
 });
