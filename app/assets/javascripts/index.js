cb10b541c99e62f9fa1d33c7976d985fc8c48f42
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-09-23 15:58:57 +0900

    fixed quotation and space

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,16 +1,17 @@
-$(document).on('turbolinks:load', function(){
+$(document).on("turbolinks:load", function(){
   $(function(){
     var search_list = $("#user-search-result");
     function appendUser(user){
       var html = `<div class="chat-group-user clearfix">
-                    <p class="chat-group-user__name">${user.name}</p>
-                    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                    <p class="chat-group-user__name">${ user.name }</p>
+                    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
                   </div>`
       search_list.append(html);
     }
+
     function appendNoUser(user){
       var html = `<div class="chat-group-user clearfix">
-                    ${user}
+                    ${ user }
                   </div>`
       search_list.append(html);
     }
@@ -18,10 +19,10 @@ $(document).on('turbolinks:load', function(){
     $("#user-search-field").on("keyup", function(){
       var input = $("#user-search-field").val();
       $.ajax({
-        type: 'GET',
-        url: '/users',
+        type: "GET",
+        url: "/users",
         data: { keyword: input },
-        dataType: 'json'
+        dataType: "json"
       })
       .done(function(users){
         $("#user-search-result").empty();
@@ -35,23 +36,23 @@ $(document).on('turbolinks:load', function(){
         }
       })
       .fail(function(){
-        alert('メンバー検索に失敗しました');
+        alert("メンバー検索に失敗しました");
       });
     });
 
     var user_list = $("#chat-group-users");
     function addUser(user_id, user_name){
       var html = `<div class="chat-group-user">
-                      <input name="group[user_ids][]" type="hidden" value="${user_id}">
-                      <p class="chat-group-user__name">${user_name}</p>
+                      <input name="group[user_ids][]" type="hidden" value="${ user_id }">
+                      <p class="chat-group-user__name">${ user_name }</p>
                       <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove">削除</a>
                   </div>`
       user_list.append(html);
     }
 
     $(document).on("click", ".chat-group-user__btn--add", function(){
-      var user_id = $(this).attr('data-user-id');
-      var user_name = $(this).attr('data-user-name');
+      var user_id = $(this).attr("data-user-id");
+      var user_name = $(this).attr("data-user-name");
       addUser(user_id, user_name);
       $(this).parent().remove();
     });
